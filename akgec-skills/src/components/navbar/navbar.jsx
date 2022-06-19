13f8182d950bf8b0467356b8d6ba321563d15741
karankulshrestha <rajprog007@gmail.com>
2022-06-20 01:55:37 +0530

    added responsive header to website

--- akgec-skills/src/components/navbar/navbar.jsx
+++ akgec-skills/src/components/navbar/navbar.jsx
@@ -3,16 +3,6 @@ import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css"
 import logo from '../../assets/logo.svg';
 
-const Menu = () => (
-  <>
-    <p><a href='#home'>Home</a></p>
-    <p><a href='#wakgec'>What is Akgec Skills?</a></p>
-    <p><a href='#programs'>Program</a></p>
-    <p><a href='#team'>Team</a></p>
-    <p><a href='#technology'>Registration</a></p>
-  </>
-)
-
 const Navbar = () => {
 
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -24,11 +14,16 @@ const Navbar = () => {
           <img src={logo} alt="logo"/>
         </div>
         <div className="akgec__navbar-links_container">
-          <Menu />
-          <div className="akgec__navbar-contact">
-          <button>Contact us</button>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#wakgec'>What is Akgec Skills?</a></p>
+          <p><a href='#programs'>Program</a></p>
+          <p><a href='#team'>Team</a></p>
+          <p><a href='#technology'>Registration</a></p>
         </div>
-        </div>
+      </div>
+
+      <div className="akgec__navbar-contact">
+          <button type='button'>Contact us</button>
       </div>
 
       <div className="akgec__navbar-menu">
@@ -39,7 +34,11 @@ const Navbar = () => {
       {toggleMenu && (
         <div className='akgec__navbar-menu__container scale-up-center'>
           <div className='akgec__navbar-menu_container-links'>
-          <Menu />
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#wakgec'>What is Akgec Skills?</a></p>
+          <p><a href='#programs'>Program</a></p>
+          <p><a href='#team'>Team</a></p>
+          <p><a href='#technology'>Registration</a></p>
           </div>
           <div className="akgec__navbar-menu_container-links-contact">
           <button>Contact us</button>
--- akgec-skills/src/containers/header/Header.jsx
+++ akgec-skills/src/containers/header/Header.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import "./header.css"
+import people from '../../assets/people.png'
+import main from '../../assets/main.svg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='akgec__header section__padding' id='home'>
+      <div className='akgec__header-content'>
+        <h1 className='gradient__text'>
+          Industrial Training Program on Embedded Systems & IoT By FabLAB AKGEC
+        </h1>
+        <h2>
+          It's time to shape your career with right and contemporary skills in technology with AKGEC Skills Foundation. 
+          If you are pursuing Engineering and desire to work in the domain of embedded systems & IoT,
+          you may join this face to face (offline) Industrial Training Program.
+        </h2>
+        <div className='akgec__header-content__people'>
+          <img src={people}/>
+          <p>More than 600 people successfully completed their training with us.</p>
+        </div>
+        <div className='akgec__header-image'>
+          <img src={main}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
