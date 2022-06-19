1a105c04b0060229ecac83ebbb80df51ec83b12e
karankulshrestha <rajprog007@gmail.com>
2022-06-19 19:57:49 +0530

    successfully added responsive navbar

--- akgec-skills/src/components/navbar/navbar.jsx
+++ akgec-skills/src/components/navbar/navbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css"
 import logo from '../../assets/logo.svg';
 
+const Menu = () => (
+  <>
+    <p><a href='#home'>Home</a></p>
+    <p><a href='#wakgec'>What is Akgec Skills?</a></p>
+    <p><a href='#programs'>Program</a></p>
+    <p><a href='#team'>Team</a></p>
+    <p><a href='#technology'>Registration</a></p>
+  </>
+)
+
 const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className='akgec__navbar'>
       <div className='akgec__navbar-links'>
@@ -11,15 +24,28 @@ const Navbar = () => {
           <img src={logo} alt="logo"/>
         </div>
         <div className="akgec__navbar-links_container">
-          <p><a href='#home'>Home</a></p>
-          <p><a href='#wakgec'>What is Akgec Skills?</a></p>
-          <p><a href='#programs'>Programs</a></p>
-          <p><a href='#technology'>Technology</a></p>
-          <p><a href='#team'>Team</a></p>
-        </div>
-        <div className="akgec__navbar-contact">
+          <Menu />
+          <div className="akgec__navbar-contact">
           <button>Contact us</button>
         </div>
+        </div>
+      </div>
+
+      <div className="akgec__navbar-menu">
+        {toggleMenu
+        ? <RiCloseLine color="#fff" size={27} onClick = { () => setToggleMenu(false)}/>
+        : <RiMenu3Line color="#fff" size={27} onClick = { () => setToggleMenu(true)}/>
+      }
+      {toggleMenu && (
+        <div className='akgec__navbar-menu__container scale-up-center'>
+          <div className='akgec__navbar-menu_container-links'>
+          <Menu />
+          </div>
+          <div className="akgec__navbar-menu_container-links-contact">
+          <button>Contact us</button>
+          </div>
+        </div>
+      )}
       </div>
     </div>
   )
