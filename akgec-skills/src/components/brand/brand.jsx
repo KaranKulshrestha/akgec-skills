fdd7df5a63f0def757ea0366c564fe9fe011f0b0
karankulshrestha <rajprog007@gmail.com>
2022-06-20 03:18:17 +0530

    added brands associated with akgec skills

--- akgec-skills/src/components/brand/brand.jsx
+++ akgec-skills/src/components/brand/brand.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import "./brand.css"
+import {aia, auto, cii, fpsi, gma, nsdc, asic} from "./imports";
+
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='akgec__brand section__padding'>
+      <div>
+        <img src={aia} alt="aia"/>
+      </div>
+      <div>
+        <img src={auto} alt="auto"/>
+      </div>
+      <div>
+        <img src={cii} alt="cii"/>
+      </div>
+      <div>
+        <img src={fpsi} alt="fpsi"/>
+      </div>
+      <div>
+        <img src={gma} alt="gma"/>
+      </div>
+      <div>
+        <img src={nsdc} alt="nsdc"/>
+      </div>
+      <div>
+        <img src={asic} alt="asic"/>
+      </div>
+    </div>
   )
 }
 
---
+++ akgec-skills/src/components/brand/imports.js
@@ -0,0 +1,17 @@
+import aia from "../../assets/aia.jpg"
+import auto from "../../assets/auto.jpg"
+import cii from "../../assets/cii.jpg"
+import fpsi from "../../assets/fpsi.jpg"
+import gma from "../../assets/gma.jpg"
+import nsdc from "../../assets/nsdc.jpg"
+import asic from "../../assets/nsic.jpg"
+
+export {
+     aia, 
+     auto, 
+     cii ,
+     fpsi ,
+     gma ,
+     nsdc ,
+     asic ,
+  };
